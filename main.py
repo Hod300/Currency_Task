1d766ba936eb57e61f09b8e1db1330b358bd3914
Hod300 <hod30020@gmail.com>
2024-01-08 19:42:14 +0200

    v1.02

--- main.py
+++ main.py
@@ -1,20 +1,12 @@
-from fastapi import FastAPI, File, UploadFile, HTTPException
-from currency import process_rates, update_daily_rates
+from fastapi import FastAPI
+from routes.currency import currency_router
 
-app = FastAPI()
+app = FastAPI(title="Backend_Api", description="Algo Backend Server", version="1.0")
 
-@app.post("/rates")
-async def process_rates_endpoint(file: UploadFile = File(...)):
-    try:
-        
-        processed_data = await process_rates(file)
-        return {"message": "Rates processed successfully", "data": processed_data}
-    except Exception as e:
-        print(e)
-        raise HTTPException(status_code=500, detail=str(e))
-    
 
-@app.put('/rates')
-async def daily_rates():
-    return update_daily_rates()
+@app.get('/')
+def check_status():
+    return 'ok'
 
+
+app.include_router(currency_router)
--- services/currency.py
+++ services/currency.py
@@ -1,35 +1,27 @@
 import pandas as pd
-from datetime import datetime, timedelta
-from fastapi import HTTPException
+from datetime import datetime
+from fastapi import HTTPException, UploadFile
 from forex_python.converter import CurrencyRates
 from io import BytesIO
 
-# Load currency mapping
-df = pd.read_csv('rates.csv')
+RATE_FILE = 'data/rates.csv'
+date_format='%m/%d/%Y'
 
-currency_df = pd.read_csv('currency.csv')
+# Load currency mapping
+currency_df = pd.read_csv('data/currency.csv')
 
 currency_mapping = {key: str(value) for key, value in zip(currency_df['EN'], currency_df['id'])}
 
-def process_file(file, format='%d/%m/%Y', excel_file=False) -> pd.DataFrame:
-    try:
-        df = pd.read_csv(file) if not excel_file else pd.read_excel(file)
-        df['Date'] = pd.to_datetime(df['Date'], format=format)
-        df.columns = [currency_mapping.get(col, col) for col in df.columns]
-        return df
-    except ValueError:
-        raise HTTPException(status_code=422, detail='Wrong format, enter a new one')
 
-async def process_rates(file):
+async def process_rates(file: UploadFile):
     if not file.filename.endswith(('.csv', '.excel')):
        raise HTTPException(422, 'Unsupported file format')
     
-    rates = process_file('rates.csv', format='%m/%d/%Y')
-    
+    df = process_file('data/rates.csv', date_format)    
     new_rates = process_file(BytesIO(await file.read()), excel_file=file.filename.endswith('.excel'))
     
     # Combine all data
-    combined_df = pd.concat([new_rates, rates], sort=False).drop_duplicates(subset='Date', keep='first')
+    combined_df = pd.concat([new_rates, df], sort=False).drop_duplicates(subset='Date', keep='first')
    
     if new_rates.empty:
         return 'data is up to date'
@@ -46,11 +38,39 @@ async def process_rates(file):
 
     cols = ['Date'] + [col for col in combined_df.columns if col != 'Date']
     combined_df = combined_df[cols].sort_values(by='Date', ascending=False)
-    
-    combined_df.to_csv('processed_rates.csv', index=False, date_format='%d/%m/%Y')
+    combined_df['Date'] = combined_df['Date'].dt.strftime(date_format)
+    combined_df.to_csv(RATE_FILE, index=False, date_format=date_format)
     return combined_df.head().fillna(0).to_dict(orient='records')
 
 
+def process_file(file, format='%d/%m/%Y', excel_file=False) -> pd.DataFrame:
+    try:
+        df = pd.read_csv(file) if not excel_file else pd.read_excel(file)
+        df['Date'] = pd.to_datetime(df['Date'], format=format)
+        df.columns = [currency_mapping.get(col, col) for col in df.columns]
+        return df
+    except ValueError:
+        raise HTTPException(status_code=422, detail='Wrong format, enter a new one')
+
+
+
+
+def update_daily_rates(base_currency='ILS') -> None:
+    df = process_file('data/rates.csv', date_format)
+    df['Date'] = df['Date'].dt.strftime(date_format)
+    all_rates = get_all_exchange_rates(base_currency)
+    converted_all_rates = {
+        str(currency_mapping.get(col, col)): round(1 / all_rates.get(col, 0), 3)
+        for col in all_rates if currency_mapping.get(col, col) in df.columns
+    } | {'Date': datetime.now().strftime(date_format)}
+
+
+    new_row = pd.Series(converted_all_rates, name=0)
+    
+    result_df = pd.concat([new_row.to_frame().T, df], ignore_index=True)
+    
+    result_df[df.columns].to_csv(RATE_FILE, index=False, date_format=date_format)
+    return converted_all_rates
 
 def get_all_exchange_rates(base_currency) -> dict:
     try:
@@ -60,18 +80,3 @@ def get_all_exchange_rates(base_currency) -> dict:
     except Exception as e:
         print(f"Error fetching rates: {e}")
         return {}
-
-def update_daily_rates(base_currency='ILS') -> None:
-    all_rates = get_all_exchange_rates(base_currency)
-    converted_all_rates = {
-        str(currency_mapping.get(col, col)): round(1 / all_rates.get(col, 0), 3)
-        for col in all_rates if currency_mapping.get(col, col) in df.columns
-    } | {'Date': datetime.now().strftime("%m/%d/%Y")}
-
-
-    new_row = pd.Series(converted_all_rates, name=0)
-    result_df = pd.concat([new_row.to_frame().T, df], ignore_index=True)
-    result_df[df.columns].to_csv("new_rates.csv", index=False)
-    return converted_all_rates
-
-
